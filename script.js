c4adca8c5f0d57ccb3e5490508e0c32bc0e6f94e
anshkunj <meghag2075@gmail.com>
2026-02-28 11:40:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const form = document.getElementById("healthForm");
 
 const analyzeBtn = document.querySelector("button[type='submit']");
-const reportBtn = document.querySelector(".report-btn");
+let reportBtn;
+
+window.addEventListener("DOMContentLoaded", ()=>{
+reportBtn = document.querySelector(".report-btn");
+});
 
 /* ===============================
    COMMON VALIDATION ENGINE
