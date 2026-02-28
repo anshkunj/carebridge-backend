8359060bda5b6d89e92074e2d1928da5c3cdbb7b
anshkunj <meghag2075@gmail.com>
2026-02-28 11:09:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -224,3 +224,21 @@ reportBtn.innerText = "📄 Download Health Report";
 }
 
 }
+
+/* ===============================
+   SCREEN TOUCH / POINTER EFFECT
+================================ */
+
+const glow = document.getElementById("touchGlow");
+
+document.addEventListener("mousemove", e=>{
+glow.style.left = e.clientX + "px";
+glow.style.top = e.clientY + "px";
+});
+
+document.addEventListener("touchmove", e=>{
+const touch = e.touches[0];
+
+glow.style.left = touch.clientX + "px";
+glow.style.top = touch.clientY + "px";
+});
